a1dabc042dba0f4f7c613f18dd1936f5cae76be9
Jacob Giberson <jacobgiberson@Jacobs-iMac.local>
2017-03-05 22:28:32 -0500

    yield

--- lambda.rb
+++ lambda.rb
@@ -12,6 +12,22 @@ ab_proc = Proc.new {|*a| a.map { |x| x + "!" }    }
 ab_proc = Proc.new {|*a| a.collect { |x| x + "!" }    }
  ab_proc[ "a", "b", "c", "d" ] #=> ["a!", "b!", "c!", "d!"]
 
+# proc.new
+a_proc = Proc.new {|a,b| (a > b) ? "a" : "b - #{b}" }
+a_proc.call(1, 2) #=> "b - 2"
+
+# lambda
+a_proc = lambda {|a,b| (a > b) ? "a" : "b - #{b}" }
+a_proc.call(1, 2) #=> "b - 2"
+
+# when to use proc.new vs lambda ?
+
+# If you want a block object that behaves like the one that Ruby generates when you pass a 
+# couple of braces into a method, use Proc.new. 
+
+# If you want something that will behave more like 
+# a regular object with a single method, use lambda
+
 
 # whats the difference between map and collect?
 (1..4).map { |i| i*i }      #=> [1, 4, 9, 16]
@@ -48,7 +64,7 @@ end
 # understanding each_cons 
 # each_cons is each consecutive
 array = ["a", "b", "c", "d", "e", "g"]
-array.each_cons(3) { |a| a.include?("a") ? (p a) : (puts "#{a} does not include 'a'") }
+# array.each_cons(3) { |a| a.include?("a") ? (p a) : (puts "#{a} does not include 'a'") }
 
 # outputs below
 # ["a", "b", "c"]
@@ -56,7 +72,7 @@ array.each_cons(3) { |a| a.include?("a") ? (p a) : (puts "#{a} does not include 
 # ["c", "d", "e"] does not include 'a'
 # ["d", "e", "g"] does not include 'a'
 
-array.each_cons(3) { |a| a.include?("b") ? (p a) : (puts "#{a} does not include 'b'") }
+# array.each_cons(3) { |a| a.include?("b") ? (p a) : (puts "#{a} does not include 'b'") }
 # ["a", "b", "c"]
 # ["b", "c", "d"]
 # ["c", "d", "e"] does not include 'b'
@@ -66,3 +82,38 @@ array.each_cons(3) { |a| a.include?("b") ? (p a) : (puts "#{a} does not include 
 m = 12.method("+")
 m.call(3)    #=> 15
 m.call(20)   #=> 32
+
+
+# understanding yield with procs
+
+# not using yield
+def print_list(array, first = 1)
+  counter = first
+  array.each do |item|
+    puts "#{counter}. #{item}"
+    # puts "#{yield counter} #{item}"
+    counter = counter.next
+  end
+end
+print_list(["Ruby", "Python", "C"]) 
+# 1. Ruby
+# 2. Python
+# 3. C
+print_list(["a","b","c"], 99)
+# 99. a
+# 100. b
+# 101. c
+
+# using yield
+def print_list(array, first = 1)
+  counter = first
+  array.each do |item|
+    # puts "#{counter}. #{item}"
+    puts "#{yield counter} #{item}"
+    counter = counter.next
+  end
+end
+print_list( [1,2,3], 23 ) { |n| "<#{n}>"}
+# <23> 1
+# <24> 2
+# <25> 3
